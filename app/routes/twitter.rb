4cf8eba23355d33da39b933d9f25539634fe2136
Dan Nguyen <dansonguyen@gmail.com>
2014-06-20 15:25:00 -0400

    before changing api structure

--- app/routes/twitter.rb
+++ app/routes/twitter.rb
@@ -8,8 +8,11 @@ module BrattyPack
 
 
       get '/twitter/users' do
-        user_ids = process_text_input_array(params['user_names'])
-        @results = @@twitter_wrapper.fetch(:users, user_ids)
+        user_names = process_text_input_array(params['user_names'].to_s)
+        # conver to real Numbers so that Twitter Client will search by userid, not screen_name
+        user_ids = process_text_input_array(params['user_ids'].to_s).map{|u| u.to_i}
+
+        @results = @@twitter_wrapper.fetch(:users, user_names + user_ids)
 
         slim :'twitter/users'
       end
